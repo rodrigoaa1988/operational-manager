d34a1a4f2aabe0679d2a0a69246cafae82bdc305
Rodrigo Almeida <rodrigoanjosalmeida@gmail.com>
2023-04-19 18:41:52 -0300

    Project created

---
+++ src/test/java/com/xmsystem/operationalmanager/OperationalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmsystem.operationalmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
