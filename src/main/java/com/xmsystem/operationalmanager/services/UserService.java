9fcbeaeadfd300ba5128165051f26262f762763a
Rodrigo Almeida <rodrigoanjosalmeida@gmail.com>
2023-04-24 15:34:43 -0300

    Service layer, component registration

---
+++ src/main/java/com/xmsystem/operationalmanager/services/UserService.java
@@ -0,0 +1,27 @@
+package com.xmsystem.operationalmanager.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.xmsystem.operationalmanager.entities.User;
+import com.xmsystem.operationalmanager.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
