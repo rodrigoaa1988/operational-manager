573859a13b51f1cd5cc9c85b1559171364f7cfad
Rodrigo Almeida <rodrigoanjosalmeida@gmail.com>
2023-04-28 20:30:24 -0300

    Order, Instant, ISO 8601 - PART 1

---
+++ src/main/java/com/xmsystem/operationalmanager/entities/Order.java
@@ -0,0 +1,82 @@
+package com.xmsystem.operationalmanager.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+	
+	public Order() {
+	}
+
+	public Order(Long id, Instant date, User client) {
+		super();
+		this.id = id;
+		this.moment = date;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getDate() {
+		return moment;
+	}
+
+	public void setDate(Instant date) {
+		this.moment = date;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/xmsystem/operationalmanager/entities/User.java
+++ src/main/java/com/xmsystem/operationalmanager/entities/User.java
@@ -1,12 +1,15 @@
 package com.xmsystem.operationalmanager.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +26,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {		
 	}
 
@@ -37,6 +43,10 @@ public class User implements Serializable {
 
 	public Long getId() {
 		return id;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public void setId(Long id) {
