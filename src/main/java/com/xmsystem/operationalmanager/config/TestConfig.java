75ba35c3c0877f6144d11ee3fc403ff89d2025ab
Rodrigo Almeida <rodrigoanjosalmeida@gmail.com>
2023-05-03 16:04:28 -0300

    OrderItem, many-to-many association with extra attributes - PARTE 2

--- src/main/java/com/xmsystem/operationalmanager/config/TestConfig.java
+++ src/main/java/com/xmsystem/operationalmanager/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.xmsystem.operationalmanager.entities.Category;
 import com.xmsystem.operationalmanager.entities.Order;
+import com.xmsystem.operationalmanager.entities.OrderItem;
 import com.xmsystem.operationalmanager.entities.Product;
 import com.xmsystem.operationalmanager.entities.User;
 import com.xmsystem.operationalmanager.enums.OrderStatus;
 import com.xmsystem.operationalmanager.repositories.CategoryRepository;
+import com.xmsystem.operationalmanager.repositories.OrderItemRepository;
 import com.xmsystem.operationalmanager.repositories.OrderRepository;
 import com.xmsystem.operationalmanager.repositories.ProductRepository;
 import com.xmsystem.operationalmanager.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +73,13 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 	}
 	
--- src/main/java/com/xmsystem/operationalmanager/entities/Order.java
+++ src/main/java/com/xmsystem/operationalmanager/entities/Order.java
@@ -2,7 +2,9 @@ package com.xmsystem.operationalmanager.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.xmsystem.operationalmanager.enums.OrderStatus;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +34,9 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 	}
@@ -76,6 +82,10 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
